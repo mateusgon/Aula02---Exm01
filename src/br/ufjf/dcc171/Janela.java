083cd438672877d6222b1c19aff31012398eeb8d
ice <ice@lab06m27.nrc.ice.ufjf.br>
2017-08-10 21:51:46 -0300

    Cria uma janela simples

---
+++ src/br/ufjf/dcc171/Janela.java
@@ -0,0 +1,12 @@
+package br.ufjf.dcc171;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+public class Janela extends JFrame {
+
+    public Janela() throws HeadlessException {
+        super("Exemplo Janela");
+    }
+    
+}
